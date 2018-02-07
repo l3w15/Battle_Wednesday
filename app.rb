7be0efde9a2d16343fc29cf9eca4e85f46565047
Lewis Watson <lewwwiswatson@gmail.com>
2018-02-07 15:04:10 +0000

    create session with player name variables

--- app.rb
+++ app.rb
@@ -1,19 +1,31 @@
 require 'sinatra/base'
 
+
+
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
+    session[:names] = [params[:Player1], params[:Player2]]
+    #
+    # @player1 = params[:Player1]
+    # @player2 = params[:Player2]
+    # session[:names] = [@player1, @player2]
+    p session
+    p session[:names]
+    redirect '/play'
+  end
+
+  get '/play' do
+    p session[:names]
+    @player1, @player2 = session[:names]
+    p @player1
     erb(:play)
   end
 
+
 end
-
-
-
-
