d63b3abedfd470335ac648dda44c79f3b501ed99
Joshua Holloway <josh.holloway@live.co.uk>
2018-02-07 12:29:23 +0000

    players entered

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-  'Testing Infrastructure working!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @player1 = params[:Player1]
+    @player2 = params[:Player2]
+    erb(:play)
   end
 
 end
+
+
+
+
--- spec/features/testing_infastructure_spec.rb
+++ spec/features/testing_infastructure_spec.rb
@@ -1,8 +1,11 @@
 require './app'
 
-feature "Checking homepage works" do
-  scenario "loading the website to see if the homepage works" do
-  visit '/'#localhost:9292
-  expect(page).to have_content 'Testing Infrastructure working!'
+feature "checking the functionality of the form" do
+  scenario "check that filled in, submitted forms, display the names on screen" do
+    visit '/'
+    fill_in('Player1', with: 'Estaban')
+    fill_in('Player2', with: 'Heisenberg')
+    click_button('Submit form')
+    expect(page).to have_content 'Estaban versus Heisenberg'
   end
 end
